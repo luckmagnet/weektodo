74a9f2548111c5ee66abdb78c038cbc6298ba348
luckmagnet <luckmagnet@duck.com>
2026-05-13 05:06:51 +0000

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import { store } from "./store/store";
-import * as Sentry from "@sentry/vue";
 
 import { createI18n } from "vue-i18n";
 import { languages } from "./assets/languages/languages.js";
@@ -14,37 +13,11 @@ const i18n = createI18n({
 
 import "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
-
 import "bootstrap-icons/font/bootstrap-icons.css";
 
 import "./assets/style/globalVars.scss";
 import "./assets/style/main.scss";
 import "./assets/style/uiComponents.scss";
-
-Sentry.init({
-  dsn: process.env.VUE_APP_SENTRY_DNS,
-  integrations: [
-    new Sentry.BrowserTracing({
-      // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
-      tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
-    })
-    // Sentry.replayIntegration({
-    //   maskAllText: true,
-    //   blockAllMedia: true,
-    // }),
-  ],
-  // Performance Monitoring
-  tracesSampleRate: 1.0, //  Capture 100% of the transactions
-  // Session Replay
-  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
-  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
-  // beforeSend(event) {
-  //   if (!store.getters.config.reportErrors) {
-  //     return null;
-  //   }
-  //   return event;
-  // },
-});
 
 const app = createApp(App);
 
